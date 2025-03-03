66c8655f1e015b38a9567e13c016c57d8d3e14f1
K24-Code <kashishvijay2405@gmail.com>
2025-03-04 00:31:44 +0530

    Add files via upload

---
+++ fill.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[4];
+   // int arr[4]={1, 2, 3, 4};
+    fill(arr,arr+4,2417);
+    cout<< arr[0] <<" "<< arr[1] <<" "<< arr[2] <<" "<< arr[3] <<endl;
+
+    return 0;
+}
